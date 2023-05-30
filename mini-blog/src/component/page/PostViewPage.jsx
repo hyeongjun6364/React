7e0dd927d569d7ad26bd8962ca2c28c0637702cd
hyung jun <lhj6364@naver.com>
2023-05-30 23:48:09 +0900

    second commit

---
+++ mini-blog/src/component/page/PostViewPage.jsx
@@ -0,0 +1,98 @@
+import React, { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import styled from "styled-components";
+import CommentList from "../list/CommentList";
+import TextInput from "../ui/TextInput";
+import Button from "../ui/Button";
+import data from "../../data.json";
+
+const Wrapper = styled.div`
+    padding: 16px;
+    width: calc(100% - 32px);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+
+const Container = styled.div`
+    width: 100%;
+    max-width: 720px;
+
+    & > * {
+        :not(:last-child) {
+            margin-bottom: 16px;
+        }
+    }
+`;
+
+const PostContainer = styled.div`
+    padding: 8px 16px;
+    border: 1px solid grey;
+    border-radius: 8px;
+`;
+
+const TitleText = styled.p`
+    font-size: 28px;
+    font-weight: 500;
+`;
+
+const ContentText = styled.p`
+    font-size: 20px;
+    line-height: 32px;
+    white-space: pre-wrap;
+`;
+
+const CommentLabel = styled.p`
+    font-size: 16px;
+    font-weight: 500;
+`;
+
+function PostViewPage(props) {
+    const navigate = useNavigate();
+    // useParams는 라우팅 매개변수를 추출하기위해 사용됨
+    // URL이 /post/123일 경우 postid는 123이 된다
+    const { postId } = useParams();
+    //id가 같은 객체가 할당됨
+    const post = data.find((item) => {
+        return item.id == postId;
+    });
+
+    const [comment, setComment] = useState("");
+
+    return (
+        <Wrapper>
+            <Container>
+                <Button
+                    title="뒤로 가기"
+                    onClick={() => {
+                        navigate("/");
+                    }}
+                />
+                <PostContainer>
+                    <TitleText>{post.title}</TitleText>
+                    <ContentText>{post.content}</ContentText>
+                </PostContainer>
+
+                <CommentLabel>댓글</CommentLabel>
+                <CommentList comments={post.comments} />
+
+                <TextInput
+                    height={40}
+                    value={comment}
+                    onChange={(event) => {
+                        setComment(event.target.value);
+                    }}
+                />
+                <Button
+                    title="댓글 작성하기"
+                    onClick={() => {
+                        navigate("/");
+                    }}
+                />
+            </Container>
+        </Wrapper>
+    );
+}
+
+export default PostViewPage;
