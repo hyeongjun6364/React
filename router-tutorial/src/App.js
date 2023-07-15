53926cb3f3736f3db0fa2bfaa6ad603244a76fe6
hyung jun <lhj6364@naver.com>
2023-07-15 15:04:05 +0900

    seven commit

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -6,18 +6,19 @@ import About from './pages/About';
 import Profile from './pages/Profile';
 import Article from './pages/Article';
 import Articles from './pages/Articles';
+import Layout from './Layout'
 function App() {
   return (
    <Routes>
-    <Route path='/' element={<Home/>} />
-    <Route path='/about' element={<About/>} />
-    <Route path='/profiles/:username' element={<Profile/>} />
+    <Route path='/' element={<Layout/>}>
+      <Route index element={<Home/>} />
+      <Route path='/about' element={<About/>} />
+      <Route path='/profiles/:username' element={<Profile/>} />
+    </Route>
     <Route path='/articles' element={<Articles/>}>
       <Route path=":id" element={<Article/>}/>
     </Route>
     
-
-
    </Routes>
   );
 }
