8b52954590abce487cf4d66e933df465b40bd4cc
hyung jun <lhj6364@naver.com>
2023-07-16 21:48:21 +0900

    final commit

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -7,6 +7,9 @@ import Profile from './pages/Profile';
 import Article from './pages/Article';
 import Articles from './pages/Articles';
 import Layout from './Layout'
+import NotFound from './pages/NotFound';
+import MyPage from './pages/MyPage';
+import Login from './pages/Login'
 function App() {
   return (
    <Routes>
@@ -18,7 +21,9 @@ function App() {
     <Route path='/articles' element={<Articles/>}>
       <Route path=":id" element={<Article/>}/>
     </Route>
-    
+    <Route path='/login' element={<Login/>}/>
+    <Route path='/mypage' element={<MyPage/>}/>
+    <Route path='*' element={<NotFound/>}/>
    </Routes>
   );
 }
--- router-tutorial/src/pages/Articles.js
+++ router-tutorial/src/pages/Articles.js
@@ -1,24 +1,40 @@
 import React from 'react'
-import {Link, Outlet} from 'react-router-dom'
+import {Link, NavLink, Outlet} from 'react-router-dom'
 function Articles() {
+    const activeStyle = {
+        color: 'green',
+        fontSize: 21,
+
+    };
+
   return (
     //Outlet을 통해 Article 컴포넌트가 보여지게 된다.
+    //Navlink는 특정 경로와 일치하면 css 클래스를 적용하는 컴포넌트이다.
     <div>
         
         <Outlet/>
         <ul>
-            <li>
-                <Link to="/articles/1">게시글 1</Link>
-            </li>
-            <li>
-                <Link to="/articles/2">게시글 2</Link>
-            </li>
-            <li>
-                <Link to="/articles/3">게시글 3</Link>
-            </li>
+            <ArticleItem id={1}/>
+            <ArticleItem id={2}/>
+            <ArticleItem id={3}/>
         </ul>
     </div>
   )
 }
 
+function ArticleItem ({id}) {
+    const activeStyle ={
+        color:'green',
+        fontSize:21,
+    }
+    return (
+        <li>
+            <NavLink
+                to={`/articles/${id}`}
+                style={({isActive}) => (isActive?activeStyle:undefined)}>
+                    게시글 {id}
+                </NavLink>
+        </li>
+    )
+}
 export default Articles
