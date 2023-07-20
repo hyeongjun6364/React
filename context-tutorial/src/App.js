4bbefb0811b56c0ae27a786c859f6d1d23115b91
hyung jun <lhj6364@naver.com>
2023-07-20 14:05:09 +0900

    second commit

---
+++ context-tutorial/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import ColorBox from './components/ColorBox';
+import ColorContext from './contexts/color';
+
+function App() {
+  return (
+    <ColorContext.Provider value={{color: 'red'}}>
+      <div>
+        <ColorBox/>
+      </div>
+    </ColorContext.Provider>
+   
+  );
+}
+
+export default App;
