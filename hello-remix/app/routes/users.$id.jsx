298c1f80cdd7dd214bfbece97a958c344174ca12
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2023-08-29 04:19:08 +0900

    ssr- remix

---
+++ hello-remix/app/routes/users.$id.jsx
@@ -0,0 +1,21 @@
+import { json } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
+import { getUser } from '../lib/api';
+import { useParams } from '@remix-run/react';
+export const loader = async ({ params }) => {
+  //const params = useParams();
+  const { id } = params;
+  const user = await getUser(id);
+  return json(user);
+};
+
+export default function User() {
+  const user = useLoaderData();
+
+  return (
+    <div>
+      <h2>{user.username}</h2>
+      <code style={{ whiteSpace: 'pre' }}>{JSON.stringify(user, null, 2)}</code>
+    </div>
+  );
+}
