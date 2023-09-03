bd7498c757ac859ac7865c0dfb21c913352a0dbb
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2023-09-04 02:44:25 +0900

    nodejs ex

---
+++ blog/blog-backend/src/index.js
@@ -0,0 +1,12 @@
+const Koa = require('Koa');
+
+const app = new Koa();
+
+app.use(ctx => {
+    ctx.body = 'hello world';
+
+});
+
+app.listen(4000,()=>{
+    console.log('Listening to port 4000');
+});
