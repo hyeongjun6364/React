0c0a2568061672e3f85efd2fd453e1bad92fd619
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2023-09-09 04:02:02 +0900

    node ex

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,11 +1,14 @@
-const Koa = require('Koa');
+const Koa = require('koa');
+const Router = require('koa-router');
 
+const api = require('./api');
 const app = new Koa();
+const router = new Router();
 
-app.use(ctx => {
-    ctx.body = 'hello world';
-
-});
+//Router 설정
+router.use('/api',api.routes());
+//app 인스턴스에 라우터 적용
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(4000,()=>{
     console.log('Listening to port 4000');
